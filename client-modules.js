139763f24e27647fba11f6bac8c2dee405829a25
SAAD-Qf <159832153+SAAD-Qf@users.noreply.github.com>
2025-12-06 21:38:27 +0500

    Add files via upload

---
+++ client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\TEXON\\Desktop\\Hackathon 1\\Physical-Humanoid-Robotics-Book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\TEXON\\Desktop\\Hackathon 1\\Physical-Humanoid-Robotics-Book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\TEXON\\Desktop\\Hackathon 1\\Physical-Humanoid-Robotics-Book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\TEXON\\Desktop\\Hackathon 1\\Physical-Humanoid-Robotics-Book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\TEXON\\Desktop\\Hackathon 1\\Physical-Humanoid-Robotics-Book\\src\\css\\custom.css"),
+];
---
+++ routes.js
@@ -0,0 +1,220 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
+    path: '/blog',
+    component: ComponentCreator('/blog', 'b2f'),
+    exact: true
+  },
+  {
+    path: '/blog/archive',
+    component: ComponentCreator('/blog/archive', '182'),
+    exact: true
+  },
+  {
+    path: '/blog/authors',
+    component: ComponentCreator('/blog/authors', '0b7'),
+    exact: true
+  },
+  {
+    path: '/blog/authors/all-sebastien-lorber-articles',
+    component: ComponentCreator('/blog/authors/all-sebastien-lorber-articles', '4a1'),
+    exact: true
+  },
+  {
+    path: '/blog/authors/yangshun',
+    component: ComponentCreator('/blog/authors/yangshun', 'a68'),
+    exact: true
+  },
+  {
+    path: '/blog/first-blog-post',
+    component: ComponentCreator('/blog/first-blog-post', '89a'),
+    exact: true
+  },
+  {
+    path: '/blog/long-blog-post',
+    component: ComponentCreator('/blog/long-blog-post', '9ad'),
+    exact: true
+  },
+  {
+    path: '/blog/mdx-blog-post',
+    component: ComponentCreator('/blog/mdx-blog-post', 'e9f'),
+    exact: true
+  },
+  {
+    path: '/blog/tags',
+    component: ComponentCreator('/blog/tags', '287'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/docusaurus',
+    component: ComponentCreator('/blog/tags/docusaurus', '704'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/facebook',
+    component: ComponentCreator('/blog/tags/facebook', '858'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/hello',
+    component: ComponentCreator('/blog/tags/hello', '299'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/hola',
+    component: ComponentCreator('/blog/tags/hola', '00d'),
+    exact: true
+  },
+  {
+    path: '/blog/welcome',
+    component: ComponentCreator('/blog/welcome', 'd2b'),
+    exact: true
+  },
+  {
+    path: '/markdown-page',
+    component: ComponentCreator('/markdown-page', '3d7'),
+    exact: true
+  },
+  {
+    path: '/docs',
+    component: ComponentCreator('/docs', 'e6e'),
+    routes: [
+      {
+        path: '/docs',
+        component: ComponentCreator('/docs', '53e'),
+        routes: [
+          {
+            path: '/docs',
+            component: ComponentCreator('/docs', 'f40'),
+            routes: [
+              {
+                path: '/docs/intro',
+                component: ComponentCreator('/docs/intro', '61d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/actuators',
+                component: ComponentCreator('/docs/physical-ai-book/actuators', 'a9f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/control',
+                component: ComponentCreator('/docs/physical-ai-book/control', '1df'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/humanoid',
+                component: ComponentCreator('/docs/physical-ai-book/humanoid', 'ccd'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/intro',
+                component: ComponentCreator('/docs/physical-ai-book/intro', '9aa'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/locomotion',
+                component: ComponentCreator('/docs/physical-ai-book/locomotion', 'ddb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/perception',
+                component: ComponentCreator('/docs/physical-ai-book/perception', 'bc7'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/planning',
+                component: ComponentCreator('/docs/physical-ai-book/planning', 'a76'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/project',
+                component: ComponentCreator('/docs/physical-ai-book/project', '909'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/ros2',
+                component: ComponentCreator('/docs/physical-ai-book/ros2', '7fc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/sensors',
+                component: ComponentCreator('/docs/physical-ai-book/sensors', '798'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/simulation',
+                component: ComponentCreator('/docs/physical-ai-book/simulation', '66b'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/physical-ai-book/vision',
+                component: ComponentCreator('/docs/physical-ai-book/vision', 'fcb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/', 'e5f'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
