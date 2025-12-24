dfbb72e76a15cee9cf61740a32afe909e42ee131
SAAD-Qf <159832153+SAAD-Qf@users.noreply.github.com>
2025-12-24 23:26:45 +0500

    Update site title and GitHub link in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Physical Humanoid Robotics Book',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -89,8 +89,8 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          href: '/Rag_chatbot',
+          label: '🤖',
           position: 'right',
         },
       ],
